e138e93aa0b8ea0566180bd685d8ff26e794aad2
nimmnisanthosh <52159154+nimmnisanthosh@users.noreply.github.com>
2022-04-21 18:10:56 +0530

    Update Helloworld.java

--- src/main/java/com/hello/Helloworld.java
+++ src/main/java/com/hello/Helloworld.java
@@ -2,7 +2,7 @@ package com.hello;
 
 public class Helloworld {
     public static void main(String args[]){
-        System.out.println("Hello World from your Maven project.");
+        System.out.println("Hello World from your Maven project1.");
     }
     public static String sayHello(){
         return "Hello";
